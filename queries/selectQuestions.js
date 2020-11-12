e44705e106a80103f44b66b9978024cc41c98cde
John Cross <john.william.cross@gmail.com>
2020-11-12 14:03:57 -0800

    put in revised query

--- queries/selectQuestions.js
+++ queries/selectQuestions.js
@@ -1,10 +1,17 @@
 const selectQuestions = `
-      SELECT 
-         *
-      FROM
-         questions
-      ORDER BY
-         ?;
+SELECT 
+questions.id AS question_id,
+questions.text AS question_text,
+questions.email AS question_email,
+questions.created_at AS question_created_at,
+answers.id AS answer_id,
+answers.text AS answer_text,
+answers.answered_at,
+answers.user_id
+FROM
+questions
+  LEFT JOIN
+answers ON questions.id = answers.question_id;
     `;
 
 module.exports = selectQuestions;
