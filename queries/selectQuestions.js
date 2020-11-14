17b43435357e8f69e085c87bb5675303befe4271
John Cross <john.william.cross@gmail.com>
2020-11-13 16:43:11 -0800

    fixed selectQuestions query

--- queries/selectQuestions.js
+++ queries/selectQuestions.js
@@ -1,19 +1,22 @@
 const selectQuestions = `
 SELECT 
-questions.id AS question_id,
-questions.text AS question_text,
-questions.email AS question_email,
-questions.created_at AS question_created_at,
-answers.id AS answer_id,
-answers.text AS answer_text,
-answers.answered_at,
-answers.user_id
+    questions.id AS question_id,
+    questions.text AS question_text,
+    questions.email AS question_email,
+    questions.created_at AS question_created_at,
+    answers.id AS answer_id,
+    answers.text AS answer_text,
+    answers.answered_at,
+    answers.user_id,
+    users.home_state AS user_home_state
 FROM
-questions
-  LEFT JOIN
-answers ON questions.id = answers.question_id
-    ORDER BY
-    ?
-    `;
+    questions
+        LEFT JOIN
+    answers ON questions.id = answers.question_id
+		LEFT JOIN
+	users ON users.id = answers.user_id
+ORDER BY
+	question_created_at DESC;
+   `;
 
 module.exports = selectQuestions;
