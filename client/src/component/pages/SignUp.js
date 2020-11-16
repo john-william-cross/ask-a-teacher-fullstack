3ada2dd7ffc0eb286f38e8797803f16d72f50e00
John Cross <john.william.cross@gmail.com>
2020-11-16 11:46:22 -0800

    commit at 28 minutes into 352A (again)

--- client/src/component/pages/SignUp.js
+++ client/src/component/pages/SignUp.js
@@ -118,6 +118,8 @@ class SignUp extends React.Component {
             password: passwordInput,
             createdAt: Date.now(),
          };
+         console.log("Created user object for POST: ", user);
+
          // post to API
          axios
             .post("/api/v1/users", user)
