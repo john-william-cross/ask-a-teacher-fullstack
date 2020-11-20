2bf3bbc3b3fa54ba5b2e996f9279c31703266ce8
John Cross <john.william.cross@gmail.com>
2020-11-20 13:03:28 -0800

    21 minutes 356A server side validation

--- api/v1/users.js
+++ api/v1/users.js
@@ -3,31 +3,38 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 const insertUser = require("../../queries/insertUser");
-const { toJson, toSafeParse, toHash } = require("../../utils/helpers");
+const { toHash } = require("../../utils/helpers");
+const getSignUpEmailError = require("../../validation/getSignUpEmailError");
+const getSignUpPasswordError = require("../../validation/getSignUpPasswordError");
 
 // @route       POST api/v1/users
 // @desc        create a new user
 // @access      PUBLIC
 router.post("/", async (req, res) => {
    const { id, email, password, createdAt } = req.body;
-   // const emailError = getSignUpEmailError(email);
-   const user = {
-      id,
-      email,
-      home_state: getHomeState(email),
-      password: await toHash(password),
-      created_at: createdAt,
-   };
+   const emailError = getSignUpEmailError(email);
+   const passwordError = getSignUpPasswordError(password);
+   if (emailError === "" && passwordError === "") {
+      const user = {
+         id,
+         email,
+         home_state: getHomeState(email),
+         password: await toHash(password),
+         created_at: createdAt,
+      };
 
-   db.query(insertUser, user)
-      .then((dbRes) => {
-         console.log(dbRes);
-         // return the user data so we can put in redux store
-      })
-      .catch((err) => {
-         console.log(err);
-         // return a 400 error to user
-      });
+      db.query(insertUser, user)
+         .then((dbRes) => {
+            console.log(dbRes);
+            // return the user data so we can put in redux store
+         })
+         .catch((err) => {
+            console.log(err);
+            // return a 400 error to user
+         });
+   } else {
+      res.status(400).json({ emailError, passwordError });
+   }
 });
 
 module.exports = router;
