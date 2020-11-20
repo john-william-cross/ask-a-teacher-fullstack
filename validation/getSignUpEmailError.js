04345b7aa1d9268eda9e6b5d88ebbd6015ca7838
John Cross <john.william.cross@gmail.com>
2020-11-20 14:07:33 -0800

    commit at 45min 356A

--- validation/getSignUpEmailError.js
+++ validation/getSignUpEmailError.js
@@ -1,4 +1,6 @@
+const db = require("../db");
 const { EMAIL_REGEX } = require("../utils/helpers");
+const selectUserByEmail = require("../queries/selectUserByEmail");
 
 module.exports = function getSignUpEmailError(email) {
    if (email === "") {
@@ -7,5 +9,21 @@ module.exports = function getSignUpEmailError(email) {
    if (EMAIL_REGEX.test(email.toLowerCase()) === false) {
       return "Please enter a valid email address";
    }
+   if (checkIsInDb(email)) {
+      return "This email already exists in the database.";
+   }
    return "";
 };
+
+function checkIsInDb(email) {
+   return db
+      .query(selectUserByEmail, email)
+      .then((users) => {
+         console.log(users);
+         if (users.length === 0) return false;
+         else return true;
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+}
