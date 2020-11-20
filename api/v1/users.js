bb51622d0390d2d52fc044fad375d10b164bc2ec
John Cross <john.william.cross@gmail.com>
2020-11-20 11:45:16 -0800

    commit at 11 min 356A destructured object

--- api/v1/users.js
+++ api/v1/users.js
@@ -3,36 +3,20 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 const insertUser = require("../../queries/insertUser");
-const selectUser = require("../../queries/selectUser");
 const { toJson, toSafeParse, toHash } = require("../../utils/helpers");
-
-// @route       GET api/v1/users
-// @desc        Get a valid user via email and password
-// @access      PUBLIC
-
-router.get("/", (req, res) => {
-   db.query(selectUser("john@nv.ccsd.net", "johjohjoh"))
-      .then((dbRes) => {
-         const user = toSafeParse(toJson(dbRes))[0];
-         console.log(user);
-         res.json(user);
-      })
-      .catch((err) => {
-         console.log(err);
-         res.status(400).json(err);
-      });
-});
 
 // @route       POST api/v1/users
 // @desc        create a new user
 // @access      PUBLIC
 router.post("/", async (req, res) => {
+   const { id, email, password, createdAt } = req.body;
+   const emailError = getSignUpEmailError(email);
    const user = {
-      id: req.body.id,
-      email: req.body.email,
-      home_state: getHomeState(req.body.email),
-      password: await toHash(req.body.password),
-      created_at: req.body.createdAt,
+      id,
+      email,
+      home_state: getHomeState(email),
+      password: await toHash(password),
+      created_at: createdAt,
    };
 
    db.query(insertUser, user)
@@ -50,7 +34,7 @@ module.exports = router;
 
 function getHomeState(email) {
    if (email.includes("nv")) {
-      return "I miss breakfast pizza.";
+      return "Iowa.";
    } else {
       return "";
    }
