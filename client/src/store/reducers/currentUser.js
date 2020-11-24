39fe5eb873d83ed84b128a836bf49a2c042cb968
John Cross <john.william.cross@gmail.com>
2020-11-24 11:04:46 -0800

    end 359A Sending and Receiving a JSON Web Token

--- client/src/store/reducers/currentUser.js
+++ client/src/store/reducers/currentUser.js
@@ -2,10 +2,14 @@
 //based on the action, it creates a new state
 
 import actions from "../actions";
+import isEmpty from "lodash/isEmpty";
 
 export default function currentUser(state = {}, action) {
    switch (action.type) {
       case actions.UPDATE_CURRENT_USER:
+         if (isEmpty(action.payload)) {
+            localStorage.removeItem("authToken");
+         }
          return action.payload;
       // (that object we got from api call)
       default:
