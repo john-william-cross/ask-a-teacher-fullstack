eafdba4845fabbcd9bbc15156d1a14cae7f1c03d
John Cross <john.william.cross@gmail.com>
2020-11-30 10:57:21 -0800

    add creatableAnswer var to store @14 min 363A

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -6,6 +6,7 @@ const initialState = {
    currentUser: {},
    allQuestions: [],
    answerableQuestion: {},
+   creatableAnswer: {},
 };
 
 const store = createStore(combineReducers, initialState, composeWithDevTools());
