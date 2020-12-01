2a739e741dd27042c0b1e55491bb951be33f3a15
John Cross <john.william.cross@gmail.com>
2020-12-01 15:56:17 -0800

    attempting to use conditional rendering in Answers.js

--- client/src/component/ui/Answer.js
+++ client/src/component/ui/Answer.js
@@ -7,12 +7,15 @@ export default function Answer(props) {
          <div className="answers" key={props.answer.id}>
             <p className="mb-4">
                {props.answer.text}
-               <span className="text-muted">
-                  This question was answered by a teacher in{" "}
-                  {props.answer.userHomeState} on{" "}
-                  {toDisplayDate(props.answer.answeredAt, "MMMM d, y")}.
-               </span>
+               {props.answer.text && (
+                  <span className="text-muted">
+                     This question was answered by a teacher in{" "}
+                     {props.answer.userHomeState} on{" "}
+                     {toDisplayDate(props.answer.answeredAt, "MMMM d, y")}.
+                  </span>
+               )}
             </p>
+
             <hr></hr>
          </div>
       </>
