c7a879e2faa3c47211716f193893f9e11b6d068f
John Cross <john.william.cross@gmail.com>
2020-12-15 08:54:04 -0800

    correctly render num of answers

--- api/v1/questions.js
+++ api/v1/questions.js
@@ -6,21 +6,25 @@ const selectQuestions = require("../../queries/selectQuestions");
 const { toJson, toSafeParse } = require("../../utils/helpers");
 const uniqBy = require("lodash/uniqBy");
 const validateJwt = require("../../utils/validateJwt");
+const { ja } = require("date-fns/locale");
 
 // @route       GET api/v1/questions
 //@desc         Get all the questions by desc order
 //@access       private
 
 //should I remove validateJwt here?
+
 router.get("/", (req, res) => {
-   // console.log(req.query);
+   // console.log("INSIDE CODEBLOCK LINE 17 HIT");
+   // console.log( req.query);
    const { order } = req.query;
+   // console.log("req.query =", req.query);
    // const userId = req.user.id;
-   // 28 minutes into 360, ask question about if I need this...
 
    /* https://www.npmjs.com/package/mysql#escaping-query-values */
    db.query(selectQuestions, [order])
       .then((questions) => {
+         // console.log("questions = ", questions);
          const questionsAndAnswers = toSafeParse(toJson(questions));
 
          const camelCasedQuestionsAndAnswers = questionsAndAnswers.map(
@@ -34,7 +38,10 @@ router.get("/", (req, res) => {
             }
          );
 
-         // console.log(camelCasedQuestionsAndAnswers);
+         // console.log(
+         //    "camelCasedQuestionsAndAnswers = ",
+         //    camelCasedQuestionsAndAnswers
+         // );
 
          const uniqQuestions = uniqBy(camelCasedQuestionsAndAnswers, `id`);
 
@@ -53,6 +60,11 @@ router.get("/", (req, res) => {
                userId: questionAndAnswer.user_id,
                userHomeState: questionAndAnswer.user_home_state,
             });
+            console.log("question.answers.id = ", question.answers.id);
+            if (question.answers[0].id === null) {
+               let removed = question.answers.splice(0, 1);
+            }
+            console.log("question.answers = ", question.answers);
          });
          // console.log(`Here are the UNIQ QUESTIONS: `, uniqQuestions);
          res.json(uniqQuestions);
